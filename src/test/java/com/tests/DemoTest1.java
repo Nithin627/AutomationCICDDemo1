5f7eb16db1bb82ef3f42a69c3235f23bf248d164
Nithin <nithinpaddyur@gmail.com>
2026-03-31 18:32:20 +0530

    upadated pom xml and test file

--- src/test/java/com/tests/DemoTest1.java
+++ src/test/java/com/tests/DemoTest1.java
@@ -2,13 +2,18 @@ package com.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class DemoTest1 {
 
 	@Test
 	public void browserTest() {
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new"); // modern headless mode
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920,1080");
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://www.google.com/");
 
 	}
