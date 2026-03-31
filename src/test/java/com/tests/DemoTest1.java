70ea14331600bb7f485154dbaee5dd71a826f054
Nithin <nithinpaddyur@gmail.com>
2026-03-31 18:02:26 +0530

    first commit

---
+++ src/test/java/com/tests/DemoTest1.java
@@ -0,0 +1,16 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	@Test
+	public void browserTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+
+	}
+
+}
